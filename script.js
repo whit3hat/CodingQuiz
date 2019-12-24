64ca327fd0e95c6b32b3724d9a2ca90c9b2aff70
Spencer Wood <spencer.wood@me.com>
2019-12-23 22:02:06 -0600

    Created questions js file

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+//Document element variables
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
 
+//Functions
+function buildQuiz()
+
+function showResults()
+
+//Displays Quiz
+buildQuiz();
+
+//On Submit show quiz results
+submitButton.addEventListener('click', showResults);
