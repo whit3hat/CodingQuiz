b8e2ae4fb7e83293d27fba7f4c4950b46b55fa09
Spencer Wood <spencer.wood@me.com>
2019-12-30 18:53:45 -0600

    edited js to clear out junk code

--- script.js
+++ script.js
@@ -1,69 +1,32 @@
-//Document element variables
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-
-//Functions
-//function buildQuiz()
-//function showResults()
-//function timer()
-
 //Displays Quiz
 
-function buildQuiz(){
-var output = [];
-//for each question
-questions.forEach(
-    (currentQuestion, questionNumber) => {
-        //list of answer choices
-        var answers = [];
-        for(choice in currentQuestion.answers){
-            //add an HTML radio button
-            answers.push(
-                `<label>
-                    <input type="radio" name="questions${questionNumber}" value="${choice}">
-                        ${choice} :
-                        ${currentQuestion.answers[choice]}
-                    </input>
-                </label>`
-            );
-        }
-        //add this question and its answers to the output
-    output.push(
-       `<div class="question">${currentQuestion.question}</div>
-        <div class="answers">${answers.join('')}</div>`
-    );
-    }
-);
-//combine our output list into one string of HTML and put on page
-quizContainer.innerHTML = output.join('');
-}
-
-//Code for Questions, loop through each one
-myQuestions.forEach( (currentQuestion, questionNumber) => {
-
-});
+(function() {
+    function buildQuiz(){
 
 //store answer choices
 var answers = [];
 
+//Code for Questions, loop through each one
+questions.forEach((currentQuestion, questionNumber) => {
+
 //each available answer
-for(letter in currentQuestion.answers){
+for(choice in currentQuestion.answers){
 
     answers.push(
         `<label>
-        <input type="radio" name="question${questionNumber}" value="${letter}">
-        ${letter} :
-        ${currentQuestion, answers[letter]}
+        <input type="radio" name="question${questionNumber}" value="${choice}">
+        ${choice} :
+        ${currentQuestion, answers[choice]}
         </label>`
     );
 }
 
 //add question and answers to output
 output.push(
-    `<div class="question">${currentQuestion, questions} </div>
-    <div class="answers">${answers.join('')}</div>`
-);
+    `<div class="question"> ${currentQuestion, questions} </div>
+    <div class="answers"> ${answers.join('')} </div>`
+  );   
+});
 
 //quiz container for HTML
 quizContainer.innerHTML = output.join('');
@@ -71,29 +34,33 @@ quizContainer.innerHTML = output.join('');
 //show results loop
 function showResults(){
     //gather answer container from quiz
-    var answerContainers = quizContainer.querySelectorAll('.answers');
+    var answerContainers = quizContainer.querySelectorAll(".answers");
+
     //keep track of answers
     let numCorrect = 0;
+
     //each question
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
+    questions.forEach((currentQuestion, questionNumber) => {
         //find selected answer
         var answerContainer = answerContainers[questionNumber];
         var selector = 'input[name=question'+questionNumber+']:checked';
         var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
         //if correct
         if(userAnswer===currentQuestion.correctAnswer){
             //add to the number of correct answers
             numCorrect++;
             //color answers green
             answerContainer[questionNumber].style.color = 'lightgreen';
-        }
-        else{
+        } else{
+            //wrong or blank
             //color the answers red
             answerContainer[questionNumber].style.color = 'red';
-        }
+          }
         });
+
         // show correct answer totoal
-        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length; 
+        resultsContainer.innerHTML = '${numCorrect} +  out of  + ${questions.length}'; 
 }
 
 //gather answer containers from quiz
@@ -102,6 +69,17 @@ var answerContainers = quizContainer.querySelectorAll('.answers');
 //track users answers
 let numCorrect = 0;
 
+//Document element variables
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener("click", showResults);
+})();
 
 //Timer
 var counter = 15;
