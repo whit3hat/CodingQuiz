c78ab61c8d8e302ae11a55c1a35c4f5a200ab9e8
Spencer Wood <spencer.wood@me.com>
2020-01-06 23:44:33 -0600

    Moved slide variables to the bottom line 109, now it shows all questions on one slide

--- script.js
+++ script.js
@@ -3,13 +3,7 @@
   const quizContainer = document.getElementById('quiz');
   const resultsContainer = document.getElementById('results');
   const submitButton = document.getElementById('submit');
-  const prevButton = document.getElementById('prev');
-  const nextButton = document.getElementById('next');
-  const slides = document.getElementById('.slide');
-    let currentSlide = 0;
-
-    showSlide(0);
-
+  
   function buildQuiz() {
     //store answer choices
     const output = [];
@@ -104,12 +98,16 @@
     showSlide(currentSlide - 1);
   }
 
-
-
-
   // display quiz right away
   buildQuiz();
 
+  const prevButton = document.getElementById('prev');
+  const nextButton = document.getElementById('next');
+  const slides = document.getElementById('.slide');
+    let currentSlide = 0;
+    
+    showSlide(0);
+  
   //on submit, show results
   submitButton.addEventListener("click", showResults);
   prevButton.addEventListener('click' , showPrev);
